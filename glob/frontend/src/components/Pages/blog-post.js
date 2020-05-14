37627ce2d78546db89d296b8e22df417e5778292
hupratt <hprattdo@gmail.com>
2020-05-14 23:23:15 +0200

    getting grid inside of react

--- glob/frontend/src/components/Pages/blog-post.js
+++ glob/frontend/src/components/Pages/blog-post.js
@@ -2,7 +2,22 @@ import React from "react";
 import Navigation from "../Elements/navigation";
 import Footer from "../Elements/footer";
 import SideBar from "../Elements/sidebar";
-import PostDetail from "./post-detail";
+
+const PostDetail = ({ post }) => {
+  if (post) {
+    return (
+      <div className="col-md-8">
+        <img src={post.header_image_url.url} className="img-fluid rounded" />
+        <hr />
+        <h1>{post.title}</h1>
+        <hr />
+        <div dangerouslySetInnerHTML={{ __html: post.md_body }} />
+      </div>
+    );
+  } else {
+    return <div className="col-md-8">Loading...</div>;
+  }
+};
 
 const BlogPost = (props) => {
   return (
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# Wagtail blog
+# React/Wagtail blog
 
 This is a demonstration project for the amazing [Wagtail CMS](https://github.com/wagtail/wagtail).
-The front end is a react-redux SPA
+The front end will (eventually) be a react-redux single page app
