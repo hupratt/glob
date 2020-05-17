1352cfddc944a3ae97442d85eba07d98b70984ac
hupratt <hprattdo@gmail.com>
2020-05-17 16:00:16 +0200

    more css fiddle

--- glob/frontend/src/components/Elements/sidebar.js
+++ glob/frontend/src/components/Elements/sidebar.js
@@ -7,6 +7,19 @@ import "./sidebar.css";
 const SideBar = ({ categories, tags, clearFilters }) => {
   return (
     <div className="col-md-4 blog-sidebar">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarResponsive"
+        aria-controls="navbarResponsive"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span>
+          <i class="fas fa-times"></i>
+        </span>
+      </button>
       <ProfileWidget />
       <CategoryWidget categories={categories} />
       <TagWidget tags={tags} clearFilters={clearFilters} />
