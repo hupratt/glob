de9f519b5085929ecb96e9637c58f41ccbc77e79
hupratt <hprattdo@gmail.com>
2020-07-10 15:36:50 +0200

    new instance ids

--- scripts/customgitpush2.sh
+++ scripts/customgitpush2.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 aws ec2 stop-instances --instance-ids i-049b26fc46b314be4
-aws ec2 stop-instances --instance-ids i-0dd6cb48c6232ede8
 
 sleep 360
 
 aws ec2 modify-instance-attribute --instance-id i-049b26fc46b314be4 --instance-type "{\"Value\": \"t2.medium\"}"
-aws ec2 modify-instance-attribute --instance-id i-0dd6cb48c6232ede8 --instance-type "{\"Value\": \"t2.medium\"}"
 
 sleep 5
 
 aws ec2 start-instances --instance-ids i-049b26fc46b314be4
-aws ec2 start-instances --instance-ids i-0dd6cb48c6232ede8
 
 sleep 60
 
@@ -23,18 +20,15 @@ git push
 sleep 200
 
 aws ec2 stop-instances --instance-ids i-049b26fc46b314be4
-aws ec2 stop-instances --instance-ids i-0dd6cb48c6232ede8
 
 sleep 360
 
 aws ec2 modify-instance-attribute --instance-id i-049b26fc46b314be4 --instance-type "{\"Value\": \"t2.micro\"}"
-aws ec2 modify-instance-attribute --instance-id i-0dd6cb48c6232ede8 --instance-type "{\"Value\": \"t2.micro\"}"
 
 sleep 5
 
 aws ec2 start-instances --instance-ids i-049b26fc46b314be4
-aws ec2 start-instances --instance-ids i-0dd6cb48c6232ede8
 
 sleep 30
 
-echo "Build: done visit https://new.pythonbots.software"
+echo "Build: done visit https://shop.lapetiteportugaise.eu/"
